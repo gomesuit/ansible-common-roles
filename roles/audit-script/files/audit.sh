cb18b191f88c6ac220da58fd9ba588161259071d
gomesuit <gomesuit@gmail.com>
2016-07-29 19:50:03 +0900

    add audit role

---
+++ roles/audit-script/files/audit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# output operation log 
+P_PROC=`ps aux | grep $PPID | grep sshd | awk '{ print $11 }'`
+if [ "$P_PROC" = sshd: ]; then
+  script -q /var/log/script/`whoami`_`date '+%Y%m%d%H%M%S'`.log
+  exit
+fi
+
