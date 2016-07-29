d31663a819feb02baf9dc4da659de082fdcbf820
gomesuit <gomesuit@gmail.com>
2016-07-30 03:21:26 +0900

    fix audit

--- roles/audit-script/files/audit.sh
+++ roles/audit-script/files/audit.sh
@@ -3,7 +3,8 @@
 # output operation log 
 P_PROC=`ps aux | grep $PPID | grep sshd | awk '{ print $11 }'`
 if [ "$P_PROC" = sshd: ]; then
-  script -q /var/log/script/`whoami`_`date '+%Y%m%d%H%M%S'`.log
+  #script -q /var/log/script/`whoami`_`date '+%Y%m%d%H%M%S'`.log
+  script -q -a -f /var/log/script/`whoami`.log
   exit
 fi
 
